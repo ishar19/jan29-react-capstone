4d0832bc6598b369525f568133bbaa462d6723cd
ishar19 <isharjain19@gmail.com>
2024-04-23 22:02:52 +0530

    inital setup and routing

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import Movies from "./pages/Movies.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/404.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
