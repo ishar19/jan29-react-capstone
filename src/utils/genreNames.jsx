5501a0c753463f472d10784b397580296c7bc5a4
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2024-04-29 19:24:51 +0530

    now using funtion to get genreNames

--- src/utils/genreNames.jsx
+++ src/utils/genreNames.jsx
@@ -1,5 +1,9 @@
-const selectedMovies = JSON.parse(localStorage.getItem("selectedMovies")); // [1,2,3]
-const allData = JSON.parse(localStorage.getItem("allData"));
-export const genreNames = selectedMovies.map((id) => {
-  return allData.find((movie) => movie.id === id);
-});
+ function getGenreNames(){
+  const selectedMovies = JSON.parse(localStorage.getItem("selectedMovies")); // [1,2,3]
+  const allData = JSON.parse(localStorage.getItem("allData"));
+  return selectedMovies.map((id) => {
+    return allData.find((movie) => movie.id === id);
+  });
+}
+
+export const genreNames = getGenreNames();
